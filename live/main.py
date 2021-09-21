230f1750cf53b0bf08f6a430789edf5b79378e56
JeyDi <andrea.guzzo92@gmail.com>
2021-09-21 10:31:27 +0200

    live code on the second meetup

--- live/main.py
+++ live/main.py
@@ -2,11 +2,37 @@ import os
 import fastapi
 import uvicorn
 from typing import Optional
+from pydantic import BaseModel
 
-api = fastapi.FastAPI()
+app = fastapi.FastAPI()
+
+product_db = {
+    1: {
+        "id": "1",
+        "name": "kebab",
+        "description": "senza cipolla",
+        "price": 4.00,
+        "available": True,
+    },
+    2: {
+        "id": 2,
+        "name": "hamburger",
+        "description": "doppio bacon",
+        "price": 6.00,
+        "available": False,
+    },
+}
 
 
-@api.get("/")
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    available: bool
+
+
+@app.get("/")
 def index():
     body = (
         "<html>"
@@ -18,25 +44,40 @@ def index():
     return fastapi.responses.HTMLResponse(content=body)
 
 
-@api.get("/default")
-def default():
-    """
-    Torniamo un default
-    """
-    return {"messaggio": "PythonBiellaGroup!"}
+@app.get("/products/")
+def get_products():
+    return product_db
 
 
-@api.get("/api/test/{x}")
-def calcola(x: int, y: int = 50, z: int = None):
-    if z == 0 or z is None:
-        return fastapi.responses.JSONResponse(content={"Errore": "Z non è valorizzato"}, status_code=400)
-    somma = x + y
-    divisione = somma / z
-    return {"x": x, "y": y, "z": z, "somma": somma, "divisione": divisione}
+@app.get("/products/{product_id}", response_model=Product)
+def get_one_products(product_id: int):
+    return product_db[product_id]
+
+
+@app.post("/products/", response_model=Product)
+def insert_product(product: Product):
+    product_db[product.id] = product
+    print(product_db)
+    return product
+
+
+@app.delete("/products/{product_id}", response_model=Product)
+def delete_product(product_id: int):
+    deleted_product = product_db[product_id]
+    product_db.pop(product_id)
+    print(product_db)
+    return deleted_product
+
+
+@app.put("/products/{product_id}", response_model=Product)
+def modify_product(product_id: int, product: Product):
+    product_db[product_id] = product
+    print(product_db)
+    return product
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('API_ENDPOINT_PORT', '8000'))
-    host = os.environ.get('API_ENDPOINT_HOST', '127.0.0.1')
+    port = int(os.environ.get("API_ENDPOINT_PORT", "8000"))
+    host = os.environ.get("API_ENDPOINT_HOST", "127.0.0.1")
 
-    uvicorn.run('main:api', port=port, host=host, reload=True)
+    uvicorn.run("main:api", port=port, host=host, reload=True)
---
+++ live/test_product.py
@@ -0,0 +1,49 @@
+from fastapi.testclient import TestClient
+from live.main import Product, app
+import pytest
+
+client = TestClient(app)
+
+
+def test_product_getall():
+    response = client.get("/products")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+def test_product_getone():
+    response = client.get("/products/1")
+    assert response.status_code == 200
+    assert response.json().get("name") == "kebab"
+
+
+def test_product_insert():
+    new_product = {
+        "id": 3,
+        "name": "hotdog",
+        "description": "ketchup",
+        "price": 1,
+        "available": True,
+    }
+    response = client.post("/products/", json=new_product)
+    assert response.status_code == 200
+    assert response.json() == new_product
+
+
+def test_product_delete():
+    product_id = 1
+    response = client.delete(f"/products/{product_id}")
+    assert response.status_code == 200
+
+
+def test_product_update():
+    product_id = 1
+    modify_product = {
+        "id": product_id,
+        "name": "kebab",
+        "description": "con piccante",
+        "price": 3.00,
+        "available": True,
+    }
+    response = client.put(f"/products/{product_id}", json=modify_product)
+    assert response.status_code == 200
